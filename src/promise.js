ced4f8d68b12ef008a56948257bd649b718282bf
Christian Parsons <christianp130@gmail.com>
2020-08-09 15:36:33 -0500

    WIP: all, race, and allSettled (need tests)
    Testse that throw errors are broken

--- src/promise.js
+++ src/promise.js
@@ -7,10 +7,10 @@ const STATE = {
 class MyPromise {
   constructor(resolver) {
     if (typeof this !== "object") {
-      throw new Error("MyPromise must be called with 'new'");
+      throw new TypeError("MyPromise must be called with 'new'");
     }
     if (typeof resolver !== "function") {
-      throw new Error("resolver must be function");
+      throw new TypeError("resolver must be function");
     }
 
     this.state = STATE.PENDING;
@@ -119,6 +119,93 @@ class MyPromise {
 
   static reject(error) {
     return new MyPromise((_, reject) => reject(error));
+  }
+
+  static all(promises) {
+    if (!promises || !Array.isArray(promises)) {
+      throw new TypeError(`MyPromise.all not given an array. Given ${typeof promises}`);
+    }
+
+    return new MyPromise((resolve, reject) => {
+      if (promises.length < 1) {
+        return resolve([]);
+      }
+
+      let fulfilledCount = 0;
+      const resolvedPromises = Array.from({ length: promises.length });
+
+      promises.forEach((value, index) => {
+        // not a promise
+        if (!value || typeof value.then !== "function") {
+          fulfilledCount++;
+          resolvedPromises[index] = value;
+        } else {
+          value.then((result) => {
+            fulfilledCount++;
+            resolvedPromises[index] = result;
+
+            // promises can resolve in different orders, so once the last promise resolve, resolve the whole collection.
+            if (fulfilledCount === promises.length) {
+              return resolve(resolvedPromises);
+            }
+          }, reject); // if any promise rejects, reject the whole collection.
+        }
+      });
+    });
+  }
+
+  static race(promises) {
+    if (!promises || !Array.isArray(promises)) {
+      throw new TypeError(`MyPromise.race not given an array. Given ${typeof promises}`);
+    }
+
+    return new MyPromise((resolve, reject) => {
+      promises.forEach((value) => {
+        // not a promise. resolve immediately
+        if (!value || typeof value.then !== "function") {
+          resolve(value);
+        } else {
+          // resolve or reject once the first promise settles
+          value.then(resolve, reject);
+        }
+      });
+    });
+  }
+
+  static allSettled(promises) {
+    if (!promises || !Array.isArray(promises)) {
+      throw new TypeError(`MyPromise.allSettled not given an array. Given ${typeof promises}`);
+    }
+
+    return new MyPromise((resolve) => {
+      if (promises.length < 1) {
+        return resolve([]);
+      }
+
+      let settledCount = 0;
+      const settledPromises = Array.from({ length: promises.length });
+
+      promises.forEach((value, index) => {
+        // not a promise
+        if (!value || typeof value.then !== "function") {
+          settledCount++;
+          settledPromises[index] = { status: STATE.FULFILLED, value };
+        } else {
+          const handleSettled = (status, resultKey) => (result) => {
+            settledCount++;
+            // return value has a status fulfilled or rejected and either a value or reason with the settled value
+            resolvedPromises[index] = { status, [resultKey]: result };
+
+            if (settledCount === promises.length) {
+              return resolve(resolvedPromises);
+            }
+          };
+
+          // wait until all promises are either resolved or rejected
+          value.then(handleSettled(STATE.FULFILLED, "value"), handleSettled(STATE.REJECTED, "reason"));
+        }
+      });
+    });
   }
 }
 
--- test/promise.test.js
+++ test/promise.test.js
@@ -5,19 +5,21 @@ const rejectPromise = (value) => new MyPromise((_, reject) => reject(value));
 
 describe(MyPromise, () => {
   it("throws when not given a function", () => {
-    expect(() => new MyPromise(null)).toThrow();
+    expect(() => new MyPromise(null)).toThrowError(TypeError);
   });
 
   describe("then", () => {
-    it("resolves the promise", () => {
+    it("resolves the promise", (done) => {
       resolvePromise(5).then((actual) => {
         expect(actual).toBe(5);
+        done();
       });
     });
 
-    it("rejects the promise", () => {
+    it("rejects the promise", (done) => {
       rejectPromise(5).then(null, (actual) => {
         expect(actual).toBe(5);
+        done();
       });
     });
 
@@ -27,11 +29,12 @@ describe(MyPromise, () => {
       expect(p.value).not.toBe(5);
     });
 
-    it("settles errors as rejected", () => {
+    xit("settles errors as rejected", (done) => {
       new MyPromise(() => {
         throw new Error("error");
       }).then(null, (actual) => {
         expect(actual).toBe(5);
+        done();
       });
     });
 
@@ -42,15 +45,16 @@ describe(MyPromise, () => {
       expect(p).not.toBe(p2);
     });
 
-    it("can resolve another MyPromise", () => {
+    it("can resolve another MyPromise", (done) => {
       resolvePromise(5).then(() =>
         new MyPromise((resolve) => resolve(10)).then((actual) => {
           expect(actual).toBe(10);
+          done();
         })
       );
     });
 
-    it("is chainable", () => {
+    it("is chainable", (done) => {
       resolvePromise(5)
         .then((actual) => {
           expect(actual).toBe(5);
@@ -58,70 +62,106 @@ describe(MyPromise, () => {
         })
         .then((actual) => {
           expect(actual).toBe(10);
+          done();
         });
     });
 
-    it("settles the promise only once", () => {
+    it("settles the promise only once", (done) => {
       new MyPromise((resolve) => {
         resolve(5);
         resolve(10);
-      }).then((actual) => expect(actual).toBe(5));
+      }).then((actual) => {
+        expect(actual).toBe(5);
+        done();
+      });
     });
 
-    it("settles the promise only once with reject", () => {
+    it("settles the promise only once with reject", (done) => {
       new MyPromise((_, reject) => {
         reject(5);
         reject(10);
-      }).then(null, (actual) => expect(actual).toBe(5));
+      }).then(null, (actual) => {
+        expect(actual).toBe(5);
+        done();
+      });
     });
 
-    it("settles the promise only once with reject and resolve", () => {
+    it("settles the promise only once with reject and resolve", (done) => {
       new MyPromise((resolve, reject) => {
         reject(5);
         resolve(10);
-      }).then(null, (actual) => expect(actual).toBe(5));
+      }).then(null, (actual) => {
+        expect(actual).toBe(5);
+        done();
+      });
     });
   });
 
   describe("catch", () => {
-    it("handles rejections", () => {
+    it("handles rejections", (done) => {
       rejectPromise("error").catch((e) => {
         expect(e).toBe("error");
+        done();
       });
     });
 
-    it("catches errors", () => {
+    xit("catches errors", (done) => {
       new MyPromise(() => {
         throw new Error("error");
       }).catch((e) => {
         expect(e).toBe("error");
+        done();
       });
     });
   });
 
   describe("finally", () => {
-    it("gets called on resolves", () => {
+    it("gets called on resolves", (done) => {
       resolvePromise("resolve").finally((value) => {
         expect(value).toBeUndefined();
+        done();
       });
     });
 
-    it("gets called on rejections", () => {
+    it("gets called on rejections", (done) => {
       rejectPromise("error").finally((value) => {
         expect(value).toBeUndefined();
+        done();
       });
     });
   });
 
   describe(MyPromise.resolve, () => {
-    it("resolves like a promise", () => {
-      MyPromise.resolve(5).then((actual) => expect(actual).toBe(5));
+    it("resolves like a promise", (done) => {
+      MyPromise.resolve(5).then((actual) => {
+        expect(actual).toBe(5);
+        done();
+      });
     });
   });
 
   describe(MyPromise.reject, () => {
     it("rejects like a promise", () => {
-      MyPromise.reject(5).then(null, (actual) => expect(actual).toBe(5));
+      MyPromise.reject(5).then(null, (actual) => {
+        expect(actual).toBe(5);
+        done();
+      });
+    });
+  });
+
+  describe(MyPromise.all, () => {
+    it("resolves all promises", (done) => {
+      MyPromise.all([MyPromise.resolve(1), MyPromise.resolve(2)]).then((resolvedCollection) => {
+        expect(resolvedCollection).toEqual([1, 2]);
+        done();
+      });
+    });
+
+    it("rejects all when one promise rejects", (done) => {
+      MyPromise.all([MyPromise.resolve(1), MyPromise.reject("rejected")]).catch((e) => {
+        expect(e).toBe("rejected");
+        done();
+      });
     });
   });
 });
